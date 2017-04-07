94b532d69b137cda75690b8683fb2386dd690a1b
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-04-07 18:01:00 -0400

    added cloze-card constructor

---
+++ cloze-card.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+
+module.exports = ClozeCard;
+
+// constructor for Cloze Card
+function ClozeCard(text, cloze){
+	this.text = text;
+	this.cloze = cloze;
+	this.clozeDelete = this.text.replace(this.cloze, "___________");
+	this.create = function() {
+		var card = {
+			text: this.text, 
+			cloze: this.cloze,
+			clozeDelete: this.clozeDelete,
+			type: "cloze"
+		};
+		// add card to card.txt
+		fs.appendFile("card.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+			if(error) {
+				console.log(error);
+			}
+		});
+	};
+}
