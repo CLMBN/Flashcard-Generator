ef8995fb3ca995b9d79d2f62270a5721d5a0c36f
CLMBN <juanmiguel.ossa+git@gmail.com>
2017-04-07 17:36:51 -0400

    initial files, BasicCard constructor

---
+++ basic-card.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+
+module.exports = BasicCard;
+
+// Constructor for the Basic Card
+function BasicCard(front, back) {
+	this.front = front;
+	this.back = back;
+	this.create = function(){
+		var info = {
+			front: this.front,
+			back: this.back,
+			type: "basic",
+		};
+		//add card to cards.txt
+		fs.appendFile("cards.txt", JSON.stringify(data) + ';', "utf8", function(error){
+			if (error) {
+				console.log(error);
+			}
+		});
+	};
+}
